26fa031f1717f2b40be2e1ae3660a02d093e4730
Arlan Nurkhozhin <dearmordor@gmail.com>
2022-12-20 22:19:48 +0100

    create Proxy pattern base

---
+++ src/main/java/cz/cvut/fel/omo/patterns/proxy/ProxyAccess.java
@@ -0,0 +1,25 @@
+package cz.cvut.fel.omo.patterns.proxy;
+
+import cz.cvut.fel.omo.model.transport.CategoryTransport;
+import cz.cvut.fel.omo.model.transport.Transport;
+import cz.cvut.fel.omo.model.user.Human;
+import cz.cvut.fel.omo.model.user.ResidentPermission;
+
+public class ProxyAccess {
+    private static final ResidentPermission allowedResidentPermission = ResidentPermission.ADULT;
+
+    public void accessAndDriveCar(Human person, Transport transport) {
+
+        if (transport.getCategoryTransport() != CategoryTransport.CAR) {
+            // LOG
+            return;
+        }
+
+        if (person.getPermissions() != ResidentPermission.ADULT) {
+            // LOG
+            return;
+        }
+
+        transport.setCurrentPerson(person);
+    }
+}
